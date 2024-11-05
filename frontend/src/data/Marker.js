5078dbfb27704e8b5c88d1c827167db4f7d47fe0
lachlaan <lachlaan@hotmail.com>
2024-11-05 21:50:14 +0100

    Update Marker.js

--- frontend/src/data/Marker.js
+++ frontend/src/data/Marker.js
@@ -41,8 +41,6 @@ export class Marker {
             
             if(this.image == "gfx/terobjs/mm/custom") {
                 icon = new ImageIcon({iconUrl: 'gfx/terobjs/mm/custom.png', iconSize: [21, 23], iconAnchor: [11, 21], popupAnchor: [1, 3], tooltipAnchor: [1, 3]})
-            } else if(this.image == 'gfx/terobjs/burrow') {
-                break;
             } else if {
                 icon = new ImageIcon({iconUrl: `${this.image}.png`, iconSize: [32, 32]});
             }
